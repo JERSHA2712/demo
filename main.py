e0041f692758df61271e2f2080f850ea0fd3ca08
JERSHA2712 <76086076+JERSHA2712@users.noreply.github.com>
2021-04-13 18:30:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import pyttsx3
 import os
 from ecapture import ecapture
 import datetime
+import subprocess
+import ctypes
+import requests
+import webbrowser
+import winshell
+
+
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.clock import Clock
+
+
 
 
 
@@ -27,6 +35,8 @@ def say(info):
     tasha.say(info)
     tasha.runAndWait()
 
+
+
 def listen():
     try:
         with sr.Microphone() as ears:
@@ -39,11 +49,20 @@ def listen():
 Builder.load_file('output_label.kv')
 
 class MyLayout(Widget):
+
     def press(self):
         task = listen()
         task = task.lower()
 
-        if 'open' in task:
+        if 'hello' in task:
+            say("Hello human")
+            self.ids.outputLabel.text = "Hello human!"
+
+        elif 'how are you' in task:
+            say('I am fine, thank you! How are you?')
+            self.ids.outputLabel.text = "I am fine, thank you! How are you?"
+
+        elif 'open' in task:
             if 'google chrome' in task:
                 say("Opening Google chrome")
                 self.ids.outputLabel.text = "Opening Google Chrome..."
@@ -86,7 +105,7 @@ class MyLayout(Widget):
                 path = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\CodeBlocks\CodeBlocks.lnk"
                 os.startfile(path)
 
-            elif 'photo shop' in task:
+            elif 'photoshop' in task:
                 say("Opening Adobe Photoshop 2020")
                 self.ids.outputLabel.text = "Opening Adobe Photoshop 2020..."
                 path = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Adobe Photoshop 2020.lnk"
@@ -98,22 +117,178 @@ class MyLayout(Widget):
                 path = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Adobe Reader 9.lnk"
                 os.startfile(path)
 
-        elif 'camera' or 'take picture' in task:
+            elif "wikipedia" in task:
+                say("Opening Wikipedia")
+                self.ids.outputLabel.text = "Opening Wikipedia..."
+                webbrowser.open("https://en.wikipedia.org/wiki/Main_Page")
+
+            elif "nucleus" in task:
+                say("Opening nucleus")
+                self.ids.outputLabel.text = "Opening Nucleus..."
+                webbrowser.open("https://nucleus.amcspsgtech.in/login")
+
+            elif "gmail" in task:
+                say("Opening Gmail")
+                self.ids.outputLabel.text = "Opening Gmail..."
+                webbrowser.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox")
+
+            elif "whatsapp" in task:
+                say("Opening WhatsApp")
+                self.ids.outputLabel.text = "Opening WhatsApp..."
+                webbrowser.open("https://web.whatsapp.com/")
+
+            elif "prime video" in task:
+                say("Opening Amazon primevideo")
+                self.ids.outputLabel.text = "Opening Amazon primevideo..."
+                webbrowser.open("https://www.primevideo.com/")
+
+            elif "youtube" in task:
+                say("Opening YouTube")
+                self.ids.outputLabel.text = "Opening YouTube..."
+                webbrowser.open("https://www.youtube.com/")
+
+            elif "stack overflow" in task:
+                say("Opening stackoverflow")
+                self.ids.outputLabel.text = "Opening stackoverflow..."
+                webbrowser.open("https://stackoverflow.com/")
+
+            elif "google" in task:
+                say("Opening Google")
+                self.ids.outputLabel.text = "Opening Google..."
+                webbrowser.open("https://www.google.com/")
+
+            else:
+                pass
+
+        elif 'play music' in task or "play song" in task:
+            say("Get ready for some grooving!")
+            mudir = r"C:\Users\Tamjid L\Music"
+            songs = os.listdir(mudir)
+            self.ids.outputLabel.text = "Now playing  " + songs[0] + "..."
+            random = os.startfile(os.path.join(mudir, songs[0]))
+
+        elif 'time' in task:
+            time = str(datetime.datetime.now())
+            hour = time[11:13]
+            min = time[14:16]
+            self.ids.outputLabel.text = hour + ":" + min
+
+            say("The time is " + hour + " " + min)
+
+        elif ' day' in task:
+            if 'today' in task:
+                day = datetime.datetime.today().weekday() + 1
+                if day == 1:
+                    today = 'Monday'
+                elif day == 2:
+                    today = 'Tuesday'
+                elif day == 3:
+                    today = 'Wednesday'
+                elif day == 4:
+                    today = 'Thursday'
+                elif day == 5:
+                    today = 'Friday'
+                elif day == 6:
+                    today = 'Saturday'
+                else:
+                    today = 'Sunday'
+                self.ids.outputLabel.text = 'Today is ' + today
+                say("It is " + today + " today.")
+
+            elif 'tomorrow' in task:
+                day = datetime.datetime.today().weekday() + 2
+                day = day % 7
+                if day == 1:
+                    tomorrow = 'Monday'
+                elif day == 2:
+                    tomorrow = 'Tuesday'
+                elif day == 3:
+                    tomorrow = 'Wednesday'
+                elif day == 4:
+                    tomorrow = 'Thursday'
+                elif day == 5:
+                    tomorrow = 'Friday'
+                elif day == 6:
+                    tomorrow = 'Saturday'
+                else:
+                    tomorrow = 'Sunday'
+                self.ids.outputLabel.text = 'Tomorrow is ' + tomorrow
+                say("It is " + tomorrow + " tomorrow.")
+            else:
+                day = datetime.datetime.today().weekday() + 1
+                if day == 1:
+                    today = 'Monday'
+                elif day == 2:
+                    today = 'Tuesday'
+                elif day == 3:
+                    today = 'Wednesday'
+                elif day == 4:
+                    today = 'Thursday'
+                elif day == 5:
+                    today = 'Friday'
+                elif day == 6:
+                    today = 'Saturday'
+                else:
+                    today = 'Sunday'
+                self.ids.outputLabel.text = 'Today is ' + today
+                say("It is " + today + " today.")
+
+        elif 'date' in task:
+            date = datetime.datetime.now().strftime("%d")
+            month = datetime.datetime.now().strftime('%B')
+            self.ids.outputLabel.text = 'Today is ' + month + " " + date
+            say("It is, " + month + " " + date)
+
+        elif 'lock window' in task:
+            self.ids.outputLabel.text = "Locking the device"
+            say("Locking the device")
+            ctypes.windll.user32.LockWorkStation()
+
+        elif 'shutdown' in task:
+            self.ids.outputLabel.text = "Shutting down"
+            say("Shutting down, miss you!")
+            subprocess.call('shutdown / p / f')
+
+        elif 'restart' in task:
+            self.ids.outputLabel.text = "Restarting"
+            say("Restarting")
+            subprocess.call(["shutdown", "/r"])
+
+        elif 'empty recycle bin' in task:
+            say("Emptying recycle bin")
+            winshell.recycle_bin().empty(confirm=True, show_progress=False, sound=True)
+            self.ids.outputLabel.text = "Recycle bin emptied..."
+
+        elif 'camera' in task or 'take picture' in task:
             say("Say cheese!")
             self.ids.outputLabel.text = "Photo taken"
             ecapture.capture(0, "photo", "img.jpg")
 
-        elif 'hello' in task:
-            say("Hello human")
-            self.ids.outputLabel.text = "Hello human!"
-
-        elif 'how are you' in task:
-            say('I am fine, thank you! How are you?')
-            self.ids.outputLabel.text = "I am fine, thank you! How are you?"
-
-        elif 'fine' or 'good':
+        elif 'fine' in task or 'good' in task:
             say("It's good to know that you are fine")
             self.ids.outputLabel.text = "It's good to know that you are fine"
+
+        elif "weather" in task:
+            say("Fetching weather report")
+            api_key = "774e65031b515e2c113ae44e18559e75"
+            base_url = "https://api.openweathermap.org/data/2.5/weather?"
+            complete_url = base_url + "q=" + "Pondicherry" + "&appid=" + api_key
+            response = requests.get(complete_url)
+            x = response.json()
+
+            if x["cod"] != "404":
+                y = x["main"]
+                current_temperature = y["temp"]
+                current_pressure = y["pressure"]
+                current_humidity = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
+                self.ids.outputLabel.text ="Temperature (in kelvin unit)= " + str(
+                    current_temperature) + "\nAtmospheric pressure (in hPa unit)= " + str(
+                    current_pressure) + "\nHumidity (in percentage)= " + str(
+                    current_humidity) + "\n\nDescription= " + str(weather_description)
+            else:
+                say(" Can't fetch at the moment!")
 
         else:
             say("Sorry, Didn't get you quite right!")
@@ -126,7 +301,6 @@ class MyApp(App):
 
 
 MyApp().run()
-
 
 
 
